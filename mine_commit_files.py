3ce2c85bd4ec9269eb774b426a66ac86465f03b1
Nguyen Vu Duc Thinh <thinhnguyen672007@gmail.com>
2026-08-23 23:31:09 +0700

    chuyển if sheet exist = replace

--- mine_commit_files.py
+++ mine_commit_files.py
@@ -53,7 +53,7 @@ for idx, row in df_commits.iterrows():
             })
 
 # Ghi bổ sung vào sheet 04_COMMIT_FILE
-with pd.ExcelWriter(excel_file, engine="openpyxl", mode="a", if_sheet_exists="overlay") as writer:
+with pd.ExcelWriter(excel_file, engine="openpyxl", mode="a", if_sheet_exists="replace") as writer:
     pd.DataFrame(commit_file_rows).to_excel(writer, sheet_name="04_COMMIT_FILE", index=False)
 
 print("\n=> HOÀN THÀNH! Đã ghi xong dữ liệu vào sheet 04_COMMIT_FILE.")
